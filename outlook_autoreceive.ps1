4e5c66440d0312a1fde0d07ecd7743af45ed3937
Sxw88 <77255430+Sxw88@users.noreply.github.com>
2023-10-22 19:07:31 +0800

    Update outlook_autoreceive.ps1
    It is finally usable ;-;

--- outlook_autoreceive.ps1
+++ outlook_autoreceive.ps1
@@ -11,11 +11,14 @@
 # Output 
 #	- downloads email attachment
 
+# Path where attachments will be downloaded
+$attachmentPath = "C:\Users\User\Downloads\"
+
 # Pre-defined criteria to match with email items in the inbox
 $criteria = @(
-	# Email Subject, 	Download Prefix, 	Download Suffix
-	@("Test Email", 	"abc", 			"def"),
-	@("Mention", 		"ghi", 			"jkl")
+	# Email Subject, 		Download Prefix
+	@("Test Email", 		"Test Email"),
+	@("Automated Email", 		"Auto Email")
 )
 
 function Scan-Mail2 {
@@ -40,11 +43,41 @@ function Scan-Mail2 {
 				$subject = $criteriaItem[0]
 				if ($emailItem.Subject -like "*$subject*") {
 					Write-Host "================ A Matching Email has been found! ================"
-					Write-Host "Sender   : $($emailItem.Sender.Name) <$($emailItem.SenderEmailAddress)>"
-					Write-Host "Rec Time : $($emailItem.ReceivedTime)"
-					Write-Host "Subject  : $($emailItem.Subject)"
-					#Write-Host "Body     : `n"
+					Write-Host "Sender     : $($emailItem.Sender.Name) <$($emailItem.SenderEmailAddress)>"
+					Write-Host "Rec Time   : $($emailItem.ReceivedTime)"
+					Write-Host "Subject    : $($emailItem.Subject)"
+					#Write-Host "Body       : `n"
 					#Write-Host $emailItem.Body
+
+     					# If attachments exist, iterate through the email and download attachments
+					$attachmentsTotalCount = $emailItem.Attachments.Count
+					if ($attachmentsTotalCount -gt 0) {
+						
+						Write-Host "Attachments:"
+						$attachmentsCount = 0
+						$dlPrefix = $criteriaItem[1]
+						
+						foreach ($attachment in $emailItem.Attachments) {
+							$attachmentsCount += 1
+							Write-Host "---`tDownloading Attachment $($attachmentsCount) out of $($attachmentsTotalCount): $($attachment.FileName)"
+							# save the attachment with the predefined download prefix $criteriaItem[1]
+							$dlFullPath = "$($attachmentPath)$($dlPrefix)_$($attachment.FileName)"
+							$dlTestPath = $dlFullPath
+							$extensionPoint = $dlFullPath.LastIndexOf(".")
+							
+							# Check if similar file exists before saving
+							$counter = 1
+							while (Test-Path $dlTestPath) {
+								# If file exists, append counter to file name
+								$dlTestPath = $dlFullPath.Substring(0, $extensionPoint) + "($($counter))" + $dlFullPath.Substring($extensionPoint)
+								$counter ++
+							}
+							
+							$attachment.SaveAsFile($dlTestPath)
+							Write-Host "   `tSaving as `"$($dlTestPath)`""
+						}
+					}
+					
 					Write-Host "`n"
 				}
 			}
